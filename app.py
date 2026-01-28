810a1245aeae8e7afaa969b0b3b15265cd93435b
edwardtalor725-web <edwardtalor725@gmail.com>
2026-01-28 18:58:16 +0700

    4

--- app.py
+++ app.py
@@ -411,4 +411,5 @@ def init_database():
 init_database()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
